df460e20f058e713a2497b562f2591b4f168539d
Vijay Kodru <123325232+vijaykodru@users.noreply.github.com>
2024-12-10 04:45:17 +0000

    code

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function fib(n) {
-    return n;
+    // Base case for n = 0
+    if (n === 0) {
+        return [0];
+    }
+    // Base case for n = 1
+    if (n === 1) {
+        return [0, 1];
+    }
+    // Recursive case to calculate the Fibonacci sequence up to n
+    else {
+        var a = fib(n - 1);
+        a.push(a[a.length - 1] + a[a.length - 2]);
+        return a;
+    }
 }
